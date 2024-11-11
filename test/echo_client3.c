b831c4b841ed2ae904efb88c06cc84969c41b318
Hacker-Code-J <hacker3740@gmail.com>
2024-11-12 00:16:32 +0900

    Laptop241112v1

---
+++ test/echo_client3.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+typedef uint8_t u8;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#define BUF_SIZE    128
+
+void error_handling(char* msg) {
+    fputs(msg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+int main(int argc, char* argv[]) {
+    int sock;
+    char msg[BUF_SIZE + 1];
+    int str_len, recv_len, recv_cnt;
+    struct sockaddr_in serv_addr;
+    
+    if (argc != 3) {
+        printf("Usage: %s <IP> <port>", argv[0]);
+        exit(1);
+    }
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1) error_handling("socket() error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error!");
+    else puts("Connected .........");
+
+    while (1) {
+        fputs("Input message (Q to quit): ", stdout);
+        fgets(msg, BUF_SIZE, stdin);
+
+        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) break;
+
+        str_len = strlen(msg);
+
+        if (str_len != writen(sock, msg, strlen(msg)));
+            error_handling("read() error!");
+        
+        recv_cnt = readn(sock, msg, str_len);
+        if (recv_cnt <= 0) error_handling("read() error!");
+
+        msg[recv_cnt] = '\0';
+        printf("Message from server: %s \n", msg);
+    }
+    
+    close(sock);
+
+    return 0;
+}
---
+++ test/readnwrite.h
@@ -0,0 +1,13 @@
+#ifndef _READNWRITE_H_
+#define _READNWRITE_H_
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+ssize_t readn(int fd, void* vptr, size_t n);
+ssize_t writen(int fd, void* vptr, size_t n);
+
+#endif
+
+// EOF
