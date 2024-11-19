ca8fccb21b4116ada8bf1feaf303691ca3052e47
Hacker-Code-J <hacker3740@gmail.com>
2024-11-19 13:42:06 +0900

    Laptop241119v1

---
+++ multi-thread/thread3.c
@@ -0,0 +1,37 @@
+// gcc -g thread3.c -o thread3 -lpthread
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int sum;
+
+void* thread_summation(void* arg) {
+    int start = ((int*)arg)[0];
+    int end = ((int*)arg)[1];
+
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+    
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t id_t1, id_t2;
+
+    int range1[] = {1, 5};
+    int range2[] = {6, 10};
+
+    pthread_create(&id_t1, NULL, thread_summation, (void*)range1);
+    pthread_create(&id_t2, NULL, thread_summation, (void*)range2);
+
+    pthread_join(id_t1, NULL);
+    pthread_join(id_t2, NULL);
+
+    printf("result: %d\n", sum);
+
+    return 0;
+}
